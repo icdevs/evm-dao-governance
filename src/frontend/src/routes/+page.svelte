a9ec4c07d1ce694a6ac9d40b81ba3dc1098cbf12
Gekctek <gekctek@edjcase.com>
2025-08-12 11:44:28 -0700

    Fixing misc

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -2,6 +2,22 @@
     import "../index.scss";
     import { backend } from "$lib/canisters";
     import { onMount } from "svelte";
+    import { browser } from "$app/environment";
+
+    let initialized = false;
+
+    onMount(() => {
+        if (browser) {
+            // Only initialize canister interactions on the client side
+            initialized = true;
+        }
+    });
 </script>
 
-<main>Test</main>
+<main>
+    {#if initialized}
+        Test - Canister Ready
+    {:else}
+        Test - Loading...
+    {/if}
+</main>
